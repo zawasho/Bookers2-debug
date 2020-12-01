74a28a3496e59802887f5f00ee345e7708fcc6d6
zawasho <shoozawa20@gmail.com>
2020-12-01 09:12:14 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users, only: [:show,:index,:edit,:update]
+  resources :books
+  root 'homes#top'
+  get 'home/about' => 'homes#about', as: 'about'
+end

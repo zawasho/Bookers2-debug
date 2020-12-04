fd27096394831b12dca65920c2923a3ccfd8c6f5
zawasho <shoozawa20@gmail.com>
2020-12-04 08:28:52 +0000

    favorite,comment 修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,19 @@
 class FavoritesController < ApplicationController
-  
+
   def create
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
-    favorite.save
-    redirect_to book_path(book)
+    if favorite.save
+      redirect_to request.referer
+    else
+      render 'books/show'
+    end
   end
 
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
 		favorite.destroy
-		redirect_to book_path(book)
+		redirect_to request.referer
   end
 end
